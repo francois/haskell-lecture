eb2517fa51492082feba987eee0ef1104b1593c8
François Beausoleil <francois@teksol.info>
2013-01-22 15:18:17 -0500

    3rd implementation of factorial using product, a function in Prelude

--- lecture-2/fac.hs
+++ lecture-2/fac.hs
@@ -2,3 +2,8 @@ facA 0 = 1
 facA n = n * facA ( n - 1 )
 
 facB n = foldl (*) 1 [ f | f <- [1..n] ]
+
+-- Even better!
+facC n = product [1..n]
+
+-- NOTE: There need to be a guard / assertion: factorial for negative numbers is undefined
